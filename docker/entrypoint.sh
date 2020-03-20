d52044e4d6652f5100a2372b82ce091e896f95a7
Tangston311 <47066967+Tangston311@users.noreply.github.com>
2020-03-20 10:17:56 -0600

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Generate the config file from environment variables
-echo "{\"app_secret\":\"$APP_SECRET\",\"pass_reqs\":$PASS_REQS,\"serial_tty\":\"$SERIAL_TTY\",\"serial_socket\":\"$SERIAL_SOCKET\"}" > /upperlevelinfinitude/infinitude.json
+echo "{\"app_secret\":\"$APP_SECRET\",\"pass_reqs\":$PASS_REQS,\"serial_tty\":\"$SERIAL_TTY\",\"serial_socket\":\"$SERIAL_SOCKET\"}" > /mainlevelinfinitude/infinitude.json
 
 if [ -z ${MODE+x} ]; then 
 	echo "MODE is not set.  Defaulting to Production";
@@ -10,4 +10,4 @@ else
 	echo "MODE is set to $MODE"; 
 fi
 
-cd /upperlevelinfinitude && ./infinitude daemon -l http://:8000
+cd /mainlevelinfinitude && ./infinitude daemon -l http://:8000
